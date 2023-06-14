3bf52bf005299aafa3bbf38537d123f2b263a3e8
Jyothi Swaroop Makena <joeswaroopmakena@gmail.com>
2023-06-15 02:30:04 +0530

    Initial commit of the portfolio website

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
-import '@/styles/globals.css'
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
+
+import { Montserrat } from "next/font/google";
+import Head from "next/head";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+});
+console.log(montserrat);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main
+        className={`${montserrat.className} bg-light dark:bg-dark min-h-screen w-full`}
+      >
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+    </>
+  );
 }
